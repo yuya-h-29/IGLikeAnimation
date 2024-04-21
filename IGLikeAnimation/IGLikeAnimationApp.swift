7568c935aa7636d9babcb712f7f30f41cd36e56e
yuya-h-29 <tzou11291996@gmail.com>
2024-04-21 12:54:49 +0900

    Initial Commit

---
+++ IGLikeAnimation/IGLikeAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  IGLikeAnimationApp.swift
+//  IGLikeAnimation
+//
+//  Created by Yuya Harada on 2024/04/21.
+//
+
+import SwiftUI
+
+@main
+struct IGLikeAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
